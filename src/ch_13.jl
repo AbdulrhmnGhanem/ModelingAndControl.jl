7829c769ca07f3d914b01900b5a1afebc669acfd
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2023-11-29 03:37:29 +0200

    (Incomplete) chapter 13

---
+++ src/ch_13.jl
@@ -0,0 +1,185 @@
+### A Pluto.jl notebook ###
+# v0.19.25
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 884380c2-2a81-11ee-35db-2bc32422997c
+# ╠═╡ show_logs = false
+begin
+	import Pkg
+	Pkg.activate("..")
+end
+
+# ╔═╡ aedd096b-8103-4f07-9cbd-827088beab6f
+using DifferentialEquations, MethodOfLines, ModelingToolkit, DomainSets, Plots, NonlinearSolve
+
+# ╔═╡ a5e96864-549e-49ec-8d40-9ee3bd8a5756
+md"# Partial Differential Equations"
+
+# ╔═╡ 2ae423f2-daf3-4ebe-ac41-d9347be427d4
+begin
+	@parameters t, x, y
+	@variables T(..), u(..), q(..)
+end;
+
+# ╔═╡ db1e018b-03a8-4746-8040-37848b084f77
+md"## Example 1
+
+$k\frac{\partial^2 T}{\partial x^2} - \rho c_p \frac{\partial T}{\partial t} = 0$
+
+- `T` : temperature
+- `ρ` : density
+- `cₚ`: key heat conductivity component
+- `k` : thermal conductivity
+"
+
+# ╔═╡ 5c3b9561-0a96-40f6-865f-a606082c64de
+begin
+	Dt = Differential(t)
+	Dx = Differential(x)
+	Dtt = Dt^2
+	Dxx = Dx^2
+	
+	
+	k = 666
+	L = 0.25
+	ρ = 1000
+	cₚ = 500
+	# q = 5e6
+	
+	# eq_1 = k * Dxx(T(t, x)) ~ ρ * cₚ * Dt(T(t, x)) + q
+	eq_1 = [
+		k * Dxx(T(t, x)) ~ ρ * cₚ * Dt(T(t, x)),
+	] #+ k * Dx(T(t, x))]
+	
+	
+	domain_1 = [
+		x ∈ Interval(0.0, L),
+		t ∈ Interval(0.0, 5.0)
+	]
+	
+	bcs_1 = [
+		# Inital condition
+		T(0, x) ~ 0.0,
+		Dt(T(t,0)) ~ 680,
+		T(t, L) ~ 0.0,
+		# q(0) ~ 0
+	]  
+	
+	@named wall = PDESystem(eq_1, bcs_1, domain_1, [t, x], [T(t, x)])
+	
+	wall_discretization = MOLFiniteDifference([x => 0.01], t)
+	prob_1 = discretize(wall, wall_discretization)
+	wall_model = solve(prob_1; saveat=0.5)
+	heat_arcoss_wall = wall_model[T(t, x)]
+end;
+
+# ╔═╡ 9f2252c9-72d5-47ba-82b9-7e3927e0f048
+plot(wall_model[x], heat_arcoss_wall[end, :];
+	label=nothing,
+	xlabel="Distance (m)",
+	ylabel="Temperature (°C)",
+	ylim=(0, 700)
+)
+
+# ╔═╡ 027983e4-75ef-49e1-b73b-1e632e33079d
+plot(wall_model[t], heat_arcoss_wall[end, :];
+	label=nothing,
+	xlabel="Distance (m)",
+	ylabel="Temperature (°C)",
+	ylim=(0, 700)
+)
+
+# ╔═╡ 28cad6ea-064c-4774-851d-69ff94a6461c
+wall_model[T(t, x)]
+
+# ╔═╡ 86091c22-d19e-49b2-8574-8b59326c5bb8
+heat_arcoss_wall[end, :]
+
+# ╔═╡ aed303d8-faff-4906-b5cc-3b6b1dc45ebb
+# gif(anim_1; fps=10)
+
+
+# ╔═╡ b7c252fc-3b81-48b0-b34a-8407cca31976
+md"## Example 2
+$\frac{\partial^2 T}{\partial x^2} = - \frac{\partial^2 T}{\partial y^2}$
+"
+
+# ╔═╡ 6f4ec480-1bd3-4ae9-9e36-b8444a46a06b
+# sol = solve(prob_2)
+
+# ╔═╡ 3ba4a33f-f549-4d22-b2fb-2fbe0f07ec1b
+md"## Example 3
+$\frac{\partial^2 u(x,t)}{\partial t^2} = \frac{K L^2}{M} \frac{\partial^2 u(x,t)}{\partial x^2}$
+"
+
+# ╔═╡ 8599e14e-1fae-4dcc-9eea-2a0bf3dac11f
+begin
+	Dyy = Differential(y)^2
+	
+	eq = Dxx(u(x, y)) + Dyy(u(x, y)) ~ 0
+
+	temperature_left = 100
+	temperature_right = 75
+	temperature_top = 250
+	temperature_bottom = 300
+	
+	width = 1
+	height = 1
+
+	bcs = [
+		u(0.0, y) ~ temperature_left,
+		u(width, y) ~ temperature_right,
+		u(x, 0) ~ temperature_bottom,
+		u(x, height) ~ temperature_top,
+	]
+	
+	# Space and time domains
+	domains = [x ∈ Interval(0.0, width),
+	           y ∈ Interval(0.0, height)]
+	
+	@named pdesys = PDESystem([eq], bcs, domains, [x, y], [u(x, y)])
+	
+	dx = 0.1
+	dy = 0.1
+	
+	# Note that we pass in `nothing` for the time variable `t` here since we
+	# are creating a stationary problem without a dependence on time, only space.
+	discretization = MOLFiniteDifference([x => dx, y => dy], nothing, approx_order=2,
+	)
+	
+	prob = discretize(pdesys, discretization)
+	sol = NonlinearSolve.solve(prob, NewtonRaphson())
+	
+	u_sol = sol[u(x, y)]
+	
+	
+	heatmap(sol[x], sol[y], u_sol, xlabel="x values", ylabel="y values",
+	        title="Steady State Heat Equation")
+end
+
+# ╔═╡ e638dfe8-ae17-4fc6-b9ff-8fa92c4e1404
+plot(sol[x], sol[y], u_sol)
+
+# ╔═╡ a5ff5ec5-ae64-4c0c-a55b-3bdbe73a9a12
+prob
+
+# ╔═╡ Cell order:
+# ╟─a5e96864-549e-49ec-8d40-9ee3bd8a5756
+# ╠═884380c2-2a81-11ee-35db-2bc32422997c
+# ╠═aedd096b-8103-4f07-9cbd-827088beab6f
+# ╠═2ae423f2-daf3-4ebe-ac41-d9347be427d4
+# ╟─db1e018b-03a8-4746-8040-37848b084f77
+# ╠═5c3b9561-0a96-40f6-865f-a606082c64de
+# ╠═9f2252c9-72d5-47ba-82b9-7e3927e0f048
+# ╠═027983e4-75ef-49e1-b73b-1e632e33079d
+# ╠═28cad6ea-064c-4774-851d-69ff94a6461c
+# ╠═86091c22-d19e-49b2-8574-8b59326c5bb8
+# ╠═aed303d8-faff-4906-b5cc-3b6b1dc45ebb
+# ╟─b7c252fc-3b81-48b0-b34a-8407cca31976
+# ╠═6f4ec480-1bd3-4ae9-9e36-b8444a46a06b
+# ╟─3ba4a33f-f549-4d22-b2fb-2fbe0f07ec1b
+# ╠═8599e14e-1fae-4dcc-9eea-2a0bf3dac11f
+# ╠═e638dfe8-ae17-4fc6-b9ff-8fa92c4e1404
+# ╠═a5ff5ec5-ae64-4c0c-a55b-3bdbe73a9a12
---
+++ src/ch_13_agian.jl
@@ -0,0 +1,84 @@
+### A Pluto.jl notebook ###
+# v0.19.27
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0438e470-2d88-11ee-38bf-bd9d69e276e2
+# ╠═╡ show_logs = false
+begin
+	import Pkg
+	Pkg.activate("..")
+end
+
+# ╔═╡ 83826d5a-2be6-453f-93b8-e6bb3d57414a
+using DifferentialEquations, MethodOfLines, ModelingToolkit, DomainSets, Plots, NonlinearSolve
+
+
+# ╔═╡ ca632c73-3b60-43b1-b061-ef8825c570ce
+md"[The Numerical Method of Lines](https://reference.wolfram.com/language/tutorial/NDSolveMethodOfLines.html)"
+
+# ╔═╡ 5255ad70-30dc-4be6-9c68-e03a5b87493e
+begin
+	@parameters t, x
+	@variables T(..), q(..)
+	
+	Dt = Differential(t)
+	Dx = Differential(x)
+	DDx = Differential(x)^2
+	
+	k = 666
+	L = 0.25
+	ρ = 1000
+	cₚ = 500
+	q_rate = 5e6
+	
+	eq_1 = [
+			Dt(q(t)) ~ q_rate,
+			k * DDx(T(t, x)) ~ ρ * cₚ * Dt(T(t, x)) + q(t)
+	]
+	
+	domain_1 = [
+		x ∈ Interval(0.0, L),
+		t ∈ Interval(0.0, 5.0)
+	]
+	
+	bcs_1 = [
+		# Inital condition
+		T(0, x) ~ 0.0,
+		T(t,0) ~ 0.0,
+		T(t, L) ~ 0.0,
+		# q(t) ~ 1.0,
+	]
+	
+	@named sys_1 = PDESystem(eq_1, bcs_1, domain_1, [t, x], [T(t, x), q(t)])
+	
+	d = MOLFiniteDifference([x => 0.01], t)
+	prob_1 = discretize(sys_1, d)
+	sol_1 = solve(prob_1; saveat=0.5)
+end;
+
+# ╔═╡ c4e906e8-7624-4103-8707-1ff19c13753d
+@gif for (i, t_desc) in enumerate(sol_1[t])
+	plot(sol_1[x], sol_1[T(t, x)][i,:];
+		ylim=(0, 700),
+		label=nothing,
+		xlabel="distance (m)",
+		ylabel="temperature (°C)",
+		title="t = $t_desc"
+	)
+end fps=10
+
+# ╔═╡ 3ffb3191-4e9e-4994-b44b-9fca571f1157
+plot(sol_1[x], sol_1[T(t, x)][end,:];
+	ylim=(0, 700),
+	label=nothing,
+)
+
+# ╔═╡ Cell order:
+# ╠═0438e470-2d88-11ee-38bf-bd9d69e276e2
+# ╠═ca632c73-3b60-43b1-b061-ef8825c570ce
+# ╠═83826d5a-2be6-453f-93b8-e6bb3d57414a
+# ╠═5255ad70-30dc-4be6-9c68-e03a5b87493e
+# ╠═c4e906e8-7624-4103-8707-1ff19c13753d
+# ╠═3ffb3191-4e9e-4994-b44b-9fca571f1157
