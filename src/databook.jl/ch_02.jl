e9069aca43bb748e7fa2cc3d5073d0323c62422f
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2024-03-14 17:35:19 +0200

    databook: 2.3

--- src/databook.jl/ch_02.jl
+++ src/databook.jl/ch_02.jl
@@ -190,12 +190,44 @@ on a large domain with an initial condition $u(x, 0) = sech(x)$. Plot the evolut
 # ╔═╡ 5232e813-eb09-4af1-bb0f-931c6febf0d2
 begin
 	function solve_three()
+		L = 100.0
+		N = 1000
+		dx = L / N
+		domain = range(-L/2, stop=L/2-dx, length=N)
+
+		κ = (2π / L) * (-N/2:N/2-1)
+		κ = fftshift(κ, 1)
+		u₀ = sech.(collect(domain))
 		
-	end
-	
-	function plot_three()
+		tspan = (0, 2.5)
+
+		function rhs(dudt, u, p, t)
+			û = fft(u)
+			dû = im * κ .* û
+			dddû = -im * (κ .^ 3) .* û
+			du = ifft(dû)
+			dddu = ifft(dddû)
+			dudt .= real(u .* du + dddu)
+		end
+		t = tspan[1]:tspan[2]/100:tspan[2]
+			
+		prob = ODEProblem(rhs, u₀, tspan)
+		u = solve(prob, saveat=t, reltol=1e-10, abstol=1e-10)
+		sol, t = u[1:end, 1:end], u.t
+
+
+		@gif for i in 1:length(eachcol(sol))
+			plot(domain, sol[:,i];
+				ylims=(-0.25, 1),
+				legend=false,
+				title="t = $(round(t[i] *1000; digits=2)) ms",
+			)
+		end
 	end
 end
+
+# ╔═╡ 24cecda6-49ba-4958-b83a-e0cd579eebf3
+solve_three()
 
 # ╔═╡ 686fcd81-b13b-4aac-a1b0-aba51ed6a1ee
 md"## Exercise 2.4
@@ -334,6 +366,7 @@ There is a great video explaining how to actually create these impulse responses
 # ╟─8b23a4c4-fb49-4e73-b833-538c8a992d54
 # ╟─02bfe122-c0fa-461c-9531-168b4f09ec1f
 # ╠═5232e813-eb09-4af1-bb0f-931c6febf0d2
+# ╠═24cecda6-49ba-4958-b83a-e0cd579eebf3
 # ╟─686fcd81-b13b-4aac-a1b0-aba51ed6a1ee
 # ╠═81bafae5-e88d-41d7-9835-f92592a71e0e
 # ╟─bc581a79-8649-4aa1-a604-5d5bee07ac13
